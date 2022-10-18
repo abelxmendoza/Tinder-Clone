690c31e76f1c000588be5e76a1b35cf0f9a11fc1
Abel Mendoza <abelxmendoza@gmail.com>
2022-10-17 19:11:08 -0700

    created a seperate repo for tinder front end to fix deployment issue

---
+++ src/TinderCards.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react'
+import './TinderCards.css'
+import TinderCard from 'react-tinder-card'
+import axios from './axios';
+
+
+function TinderCards() {
+
+    const [people, setPeople] = useState([]);
+
+    /*  
+    {
+            name: 'Elon Musk',
+            url: 'https://phantom-marca.unidadeditorial.es/eca62c8214fde70c26e664d672a38b59/resize/1320/f/jpg/assets/multimedia/imagenes/2022/08/05/16596775100672.jpg',
+        },
+        {
+            name: 'Ozzy Osborne',
+            url: 'https://ca-times.brightspotcdn.com/dims4/default/1dbc5e0/2147483647/strip/true/crop/2209x3081+0+0/resize/1200x1674!/quality/80/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F57%2F3e%2F487912c24ee6ab014f992b87527f%2Fla-photos-1staff-485126-la-et-ms-ozzy-osbourne-parkinsons-disease3-mam.jpg',
+        },
+        
+    */
+
+    useEffect(() => {
+        async function fetchData() {
+            const req = await axios.get('/tinder/cards');
+
+            setPeople(req.data)
+        }
+
+
+        fetchData();
+    }, [])
+
+    console.log(people)
+
+    const swiped = (direction, nameToDelete) => {
+        console.log('removing: ' + nameToDelete)
+        //setLastDirection(direction);
+    }
+      
+    const outOfFrame = (name) => {
+        console.log(name + ' left the screen')
+    }
+
+  return (
+    <div className='tinderCards'>
+
+        <div className='tinderCards_container'>
+
+            {people.map((person) => ( 
+
+
+               <TinderCard
+                className='swipe'
+                key = {person.name}
+                preventSwipe={["up", "down"]}
+                onSwipe={(dir) => swiped(dir, person.name)}
+                onCardLeftScreen={() => outOfFrame(person.name)}
+               >
+
+                <div className='card'
+                     style={{ backgroundImage: `url(${person.imgUrl})` }}
+                     >
+                    
+                    <h3>{person.name}</h3>
+                </div>
+
+               </TinderCard>
+
+
+            )) }
+
+        </div>
+
+        
+    </div>
+  )
+}
+
+export default TinderCards;
---
+++ src/axios.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const instance = axios.create({
+
+    baseURL: 'https://abels-tinder-backend.herokuapp.com',
+
+})
+
+
+export default instance;
